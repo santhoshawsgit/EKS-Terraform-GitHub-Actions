cec4908fadb0d5cc4ad1fbd6e033d9c4150a3e2f
santhoshawsgit <101330547+santhoshawsgit@users.noreply.github.com>
2025-09-28 19:51:06 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "pinco"
-    region         = "us-east-1"
+    region         = "ap-southeast-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
